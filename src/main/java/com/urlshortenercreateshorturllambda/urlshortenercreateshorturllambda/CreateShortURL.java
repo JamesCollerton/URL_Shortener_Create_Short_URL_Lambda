9d56bfe99cf582da50bd89779bf6f280773d4399
James Collerton <jamescollerton@live.co.uk>
2019-03-16 19:47:50 +0000

    Trying to add a main class

---
+++ src/main/java/com/urlshortenercreateshorturllambda/urlshortenercreateshorturllambda/CreateShortURL.java
@@ -0,0 +1,9 @@
+package com.urlshortenercreateshorturllambda.urlshortenercreateshorturllambda;
+
+public class CreateShortURL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
