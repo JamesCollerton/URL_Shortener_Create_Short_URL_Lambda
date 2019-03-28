9272c2adc9d1f38a100c27583acefe316fcc289e
James Collerton <jamescollerton@live.co.uk>
2019-03-28 06:51:18 +0000

    Moved Spring project in

---
+++ src/main/java/com/urlshortener/createshorturl/CreateshorturlApplication.java
@@ -0,0 +1,13 @@
+package com.urlshortener.createshorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateshorturlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateshorturlApplication.class, args);
+	}
+
+}
