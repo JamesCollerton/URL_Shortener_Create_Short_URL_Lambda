8649bcba9fcdb2b055a91daf46d07d55c8c73976
James Collerton <jamescollerton@live.co.uk>
2019-03-28 21:12:15 +0000

    Added web dependency and inner code

--- src/main/java/com/urlshortener/createshorturl/CreateshorturlApplication.java
+++ src/main/java/com/urlshortener/createshorturl/CreateshorturlApplication.java
@@ -2,9 +2,10 @@ package com.urlshortener.createshorturl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CreateshorturlApplication {
+public class CreateshorturlApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreateshorturlApplication.class, args);
