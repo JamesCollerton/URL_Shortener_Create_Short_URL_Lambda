43b318423011dec46f4ebac8b63d738d5dd7ecbd
James Collerton <jamescollerton@live.co.uk>
2019-04-02 22:25:00 +0100

    Tightened up formatting

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,21 +4,20 @@ variable "lambda_s3_bucket" {
 }
 
 variable "lambda_s3_key" {
-#  default 	= "createshorturl-0.0.1-SNAPSHOT.jar"
   description   = "Object key of the S3 bucket where we want to store our code"
   type          = "string"
 }
 
 variable "lambda_function_name" {
-  default = "url-shortener-create-short-url-lambda"
-  description = "Name we would like to assign to the lambda function"
-  type = "string"
+  default 	= "url-shortener-create-short-url-lambda"
+  description 	= "Name we would like to assign to the lambda function"
+  type 		= "string"
 }
 
 variable "lambda_function_handler" {
-  default = "URLShortenerCreateShortURL::apply"
-  description = "The name of the method invoked in the lambda, given Class::method"
-  type = "string"
+  default 	= "URLShortenerCreateShortURL::apply"
+  description 	= "The name of the method invoked in the lambda, given Class::method"
+  type 		= "string"
 }
 
 variable "iam_for_lambda_arn" {
