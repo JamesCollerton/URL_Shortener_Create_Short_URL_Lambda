f5696d39d46d16a7d2a3a6f126b93fa054f7aec1
James Collerton <jamescollerton@live.co.uk>
2019-04-05 07:10:04 +0100

    Cleaned up imports

--- src/main/java/com/urlshortener/createshorturl/CreateshorturlApplication.java
+++ src/main/java/com/urlshortener/createshorturl/CreateshorturlApplication.java
@@ -1,14 +1,8 @@
 package com.urlshortener.createshorturl;
 
-import com.urlshortener.createshorturl.controllers.LambdaController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class CreateshorturlApplication extends SpringBootServletInitializer {
