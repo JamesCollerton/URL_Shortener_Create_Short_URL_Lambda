3d7e323e7d171d9e6567bbc3b2c587a9f3fbbdb3
James Collerton <jamescollerton@live.co.uk>
2019-04-14 14:21:00 +0100

    Changed it to a POST request

--- src/main/java/com/urlshortener/createshorturl/controllers/LambdaController.java
+++ src/main/java/com/urlshortener/createshorturl/controllers/LambdaController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,7 +22,7 @@ public class LambdaController {
     @Autowired
     private URLShortener urlShortener;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<ShortenedURLInformation> get(
             @Validated @RequestBody ShortenedURLInformation shortenedURLInformation
     ) {
